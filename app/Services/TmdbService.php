baf74a67b815874f5d213c4b67dfb1ba93d7db60
Kaciano Ghelere <themohawkeagle@gmail.com>
2020-01-20 16:40:53 -0300

    Movie details added (directors, cast and videos)

--- app/Services/TmdbService.php
+++ app/Services/TmdbService.php
@@ -7,6 +7,8 @@ use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class TmdbService {
 
+    private $allowedVideoSources = ['YouTube'];
+
     public function fetchMovies($query, $page = 1) {
         return $this->fetchApiResponse('/search/movie', compact('page', 'query'));
     }
@@ -16,7 +18,50 @@ class TmdbService {
     }
 
     public function fetchMovieDetails($movieId) {
-        return $this->fetchApiResponse("/movie/{$movieId}");
+        $parameters = [
+            'append_to_response' => 'credits,videos'
+        ];
+
+        $movie = $this->fetchApiResponse("/movie/{$movieId}", $parameters);
+        $movie['main_cast'] = $this->handleMovieCastMembers($movie['credits']);
+        $movie['directors'] = $this->handleMovieDirectors($movie['credits']);
+        $movie['videos'] = $this->handleMovieVideos($movie['videos']);
+
+        unset($movie['credits']);
+
+        return $movie;
+    }
+
+    private function handleMovieCastMembers($movieCredits) {
+        $maxCastMembersCount = 8;
+
+        return array_slice($movieCredits['cast'], 0, $maxCastMembersCount);
+    }
+
+    private function handleMovieDirectors($movieCredits) {
+        $directors = [];
+
+        foreach ($movieCredits['crew'] as $member) {
+            if ($member['job'] === 'Director') {
+                $directors[] = $member;
+            }
+        }
+
+        $maxDirectorsCount = 3;
+
+        return array_slice($directors, 0, $maxDirectorsCount);
+    }
+
+    private function handleMovieVideos($movieVideos) {
+        $videos = [];
+
+        foreach ($movieVideos['results'] as $video) {
+            if (in_array($video['site'], $this->allowedVideoSources)) {
+                $videos[] = $video;
+            }
+        }
+
+        return $videos;
     }
 
     private function fetchApiResponse($urlAppend = '', $params = []) {
@@ -50,6 +95,6 @@ class TmdbService {
             throw new HttpException(Response::HTTP_INTERNAL_SERVER_ERROR, $error);
         }
 
-        return json_decode($response);
+        return json_decode($response, true);
     }
 }
